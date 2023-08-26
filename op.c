7127bedeb79302a8f23b772693450e0a63f24755
KelvinoKing <kelvinoking@outlook.com>
2023-08-26 07:24:56 +0000

    Updated pop function

--- op.c
+++ op.c
@@ -92,7 +92,7 @@ void pop(stack_t **head, unsigned int line_num)
 
 	if (*head == NULL || head == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack", line_num);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 	temp = *head;
